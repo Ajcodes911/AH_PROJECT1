65d8f1e3c2fbc8cade8fd2c0f96b5032b7fb18ef
Ajcode911 <ayushp@op.iitg.ac.in>
2026-05-01 13:20:28 +0530

    Notes till operators

--- PythonLessons.py
+++ PythonLessons.py
@@ -23,5 +23,56 @@ print(12/3) #converts int to float """""
 Name="Ayush"
 age="22"
 print("My name is",Name,"My age is:",age)'''
-
+"""
 print("Dropping python stuffs")
+"""
+#Implicit and explicit type conversion:
+"""Implicit type conversion is when python automatically changes the data:
+a=12
+print(12/3)
+it gives output 4.0 which is float so this is implicit type conversion as python converted an int to float"""
+
+#Operator in python:
+'''
+ a=5
+ b=10
+ print(b/a)
+ The output will be 2.0 because in python fractions gives outout in float .
+ #To get only 2:
+ We use floor division :
+ print(b//a)'''
+"""
+a=5
+b=32
+print(b/a) #The output is 6.4
+print(b//a) #The output is 6 """
+#Modulus %:
+""" print(32%5) #The output will be the remainder: 2"""
+
+#Assignment operator:
+""" a=23 #Assigning value
+COMPOUND ASSIGNMENT  OPERATOR:
+
+a=20
+a +=20 
+print(a) """
+#Other compound operators:
+# -=
+# /= ......etc
+""" Comparison operators always gives output inT or F """
+#How to find Unicode:
+"""
+print(ord("A"))#65 
+print(ord("B")) #66
+print("A">"B")#False"""
+# print("ABC">"ACD")#FALSE as compare letter by letter
+#Comparison can be only done between similar data types str to str and int to int etc
+
+#QUESTIONS:Answer True and False:
+""" print(126>130) #False
+print((456==456) != (235==236)) #True
+print(12<10 or 45==56 or 69>70 or 15 !=13) #True
+print(True and bool(0)) #False"""
+#CONDITIONAL STATEMENTS
+
+
