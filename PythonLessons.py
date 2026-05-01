720d8433706947f596c3bf3e7d42a74e10de396c
Ajcode911 <ayushp@op.iitg.ac.in>
2026-05-01 12:15:17 +0530

    Python notes

---
+++ PythonLessons.py
@@ -0,0 +1,27 @@
+'''print("Hello Python")
+print("Hi Ayush")
+"""This is multiline comment"""
+a="Learning Python"
+print(a[0:9:3])'''
+
+'''a=12 #Integer
+print(type(a))#gives int
+a=str(a)
+print(type(a)) #gives str'''
+
+"""a="Hello"
+print(bool(a))"""
+
+#Falsey Values in Bool(7):
+"""Falsey,false,0,0.0,"empty string",{empty tuple},[]"""
+
+"""Implicit Type conversion is when python automatically converts a data type"""
+"""Ex:a=12
+print(12/3) #converts int to float """""
+#Formated and Raw string :
+'''
+Name="Ayush"
+age="22"
+print("My name is",Name,"My age is:",age)'''
+
+print("Dropping python stuffs")
